645ab37c4d5ce26e3b006b52f489555cdd868fa7
Joe Martucci <joe.martucci@gmail.com>
2025-01-14 20:07:46 -0500

    Test revalidation on home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import prisma from "../db";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import SearchResults from "@/components/SearchResults/SearchResults";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const gifs = await prisma.gif.findMany({
     skip: 0,
