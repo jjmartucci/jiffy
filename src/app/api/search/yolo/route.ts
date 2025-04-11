a4c646e05c553f6673b2353296507687d11ac10b
Joe Martucci <joe.martucci@gmail.com>
2025-04-11 16:18:49 -0400

    Update the yolo search.

--- src/app/api/search/yolo/route.ts
+++ src/app/api/search/yolo/route.ts
@@ -1,41 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/db";
-import path from "path";
-import fs from "fs/promises";
-import lunr from "lunr";
 import { createUrl } from "@/app/utilities/gifurl";
 
+/** Get a single url based on a search parameter */
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("q");
-  const indexPath = path.join(process.cwd(), `search`, "index.json"); // Make sure this "uploads" directory exists
 
-  try {
-    await fs.access(indexPath);
-  } catch {
-    console.log("No index exists, let's make one.");
-    await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/api/search/updateIndex`, {
-      method: "POST",
-    });
-  }
+  // just hit the internal search and return the first result
+  const req = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/api/search?query=${query}`, {
+    method: "GET",
+  });
 
-  const data = await fs.readFile(indexPath, "utf8");
+  const gifs = await req.json()
+  console.log(gifs.gifs[0])
 
-  const idx = lunr.Index.load(JSON.parse(data).index);
-  const results = idx.search(query);
-
-  if (results.length === 0) {
+  if (gifs.length === 0) {
     return NextResponse.json({
       gif: "",
       status: 200,
     });
   }
 
-  const bestMatch = await prisma.gif.findFirst({
-    where: {
-      id: results[0].ref,
-    },
-  });
+  const bestMatch =  gifs.gifs[0];
 
   const imageUrl = createUrl(
     process.env.NEXT_PUBLIC_IMAGE_HOST_URL,
