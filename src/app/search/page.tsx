ef93ab4af0d7e7750911439ca5ad632c80cdbd72
Joseph Martucci <joe@Josephs-MBP.localdomain>
2025-08-07 11:46:56 -0400

    Remove unused variables.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,61 +1,60 @@
 import SearchBar from "@/components/SearchBar/SearchBar";
 import SearchResults from "@/components/SearchResults/SearchResults";
 import noResults from "@/images/no-results.gif";
-import { Center, Space, Stack, Title } from "@mantine/core";
+import {Center, Space, Stack, Title} from "@mantine/core";
 import Image from "next/image";
 
-import type { Metadata } from 'next'
+import type {Metadata} from 'next'
 
 export async function generateMetadata(
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
+    {searchParams}: Props,
 ): Promise<Metadata> {
-  const query = (await searchParams).q;
+    const query = (await searchParams).q;
 
-  return {title: `Jiffy - Search results for ${query}`}
+    return {title: `Jiffy - Search results for ${query}`}
 }
 
 export default async function Search({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+                                         searchParams,
+                                     }: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const query = (await searchParams).q;
+    const query = (await searchParams).q;
 
-  const callSearch = async (searchString: string) => {
-    const searchURL = new URL("/api/search", process.env.NEXT_PUBLIC_BASE_PATH);
-    searchURL.searchParams.set("query", searchString);
-    const searchFetch = await fetch(searchURL.toString());
-    const response = await searchFetch.json();
+    const callSearch = async (searchString: string) => {
+        const searchURL = new URL("/api/search", process.env.NEXT_PUBLIC_BASE_PATH);
+        searchURL.searchParams.set("query", searchString);
+        const searchFetch = await fetch(searchURL.toString());
+        const response = await searchFetch.json();
 
-    return response.gifs;
-  };
+        return response.gifs;
+    };
 
-  const searchResults = await callSearch(query);
-  const count = searchResults.length;
-  return (
-    <>
-      <SearchBar />
-      {count > 0 && (
+    const searchResults = await callSearch(query);
+    const count = searchResults.length;
+    return (
         <>
-          <Title order={2}>{`${count} ${
-            count === 1 ? "gif" : "gifs"
-          } found for ${query} `}</Title>
-          <Space h="xl" />
-        </>
-      )}
+            <SearchBar/>
+            {count > 0 && (
+                <>
+                    <Title order={2}>{`${count} ${
+                        count === 1 ? "gif" : "gifs"
+                    } found for ${query} `}</Title>
+                    <Space h="xl"/>
+                </>
+            )}
 
-      <SearchResults searchResults={searchResults} />
-      {count === 0 && (
-        <div>
-          <Center>
-            <Stack>
-              <Title order={2}>{`No results for ${query}`}</Title>
-              <Image src={noResults} alt="No results found" unoptimized />
-            </Stack>
-          </Center>
-        </div>
-      )}
-    </>
-  );
+            <SearchResults searchResults={searchResults}/>
+            {count === 0 && (
+                <div>
+                    <Center>
+                        <Stack>
+                            <Title order={2}>{`No results for ${query}`}</Title>
+                            <Image src={noResults} alt="No results found" unoptimized/>
+                        </Stack>
+                    </Center>
+                </div>
+            )}
+        </>
+    );
 }
